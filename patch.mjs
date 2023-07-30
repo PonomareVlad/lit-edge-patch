5b3945e55cd38bb51615d594f6cbcc55b2c169a2
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-07-31 02:52:59 +0500

    entities package patching

--- patch.mjs
+++ patch.mjs
@@ -1,9 +1,10 @@
-const p1 = './node_modules/lit-html/package.json'
-const p2 = './node_modules/@lit/reactive-element/package.json'
+const p1 = './node_modules/entities/package.json'
+const p2 = './node_modules/lit-html/package.json'
+const p3 = './node_modules/@lit/reactive-element/package.json'
 
 import fs from 'fs'
 
-for (const p of [p1, p2]) {
+for (const p of [p1, p2, p3]) {
   // for all exports path, copy the node condition as worker condition, put it before the worker condition
   const code = fs.readFileSync(p, 'utf8')
   const json = JSON.parse(code)
